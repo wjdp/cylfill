8ba2074ba89ef2bb16de5e790ebffd6f4820ba7f
Will Pimblett <will@wjdp.uk>
2022-05-21 23:03:19 +0100

    Add icons to pwa

--- vite.config.ts
+++ vite.config.ts
@@ -8,12 +8,35 @@ export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      includeAssets: ["favicon.ico", "robots.txt"],
+      includeAssets: [
+        "cylfill-rect.png",
+        "cylfill-logo-192.png",
+        "cylfill-logo-512.png",
+        "robots.txt",
+      ],
       manifest: {
         name: "cylfill",
         short_name: "cylfill",
         description: "Tiny app to track filling of compressed air cylinders",
         theme_color: "#0F1629",
+        icons: [
+          {
+            src: "cylfill-logo-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "cylfill-logo-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "cylfill-logo-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
       },
       workbox: {
         globPatterns: ["assets/*", "*.html", "manifest.webmanifest"],
